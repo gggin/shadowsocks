e2500f22c155797793ed3068859240d520195cb0
gggin <fm369o802340@gmail.com>
2016-01-28 07:38:28 +0000

    .

--- shadowsocks/Config.py
+++ shadowsocks/Config.py
@@ -1,11 +1,11 @@
 #Config
-MYSQL_HOST = 'mdss.mengsky.net'
+MYSQL_HOST = '127.0.0.1'
 MYSQL_PORT = 3306
-MYSQL_USER = 'ss'
-MYSQL_PASS = 'ss'
+MYSQL_USER = 'root'
+MYSQL_PASS = '123456'
 MYSQL_DB = 'shadowsocks'
 
-MANAGE_PASS = 'ss233333333'
+MANAGE_PASS = 'ss23sdf3333'
 #if you want manage in other server you should set this value to global ip
 MANAGE_BIND_IP = '127.0.0.1'
 #make sure this port is idle
